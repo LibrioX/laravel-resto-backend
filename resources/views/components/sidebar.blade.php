dbc96ab6651662917e72f14aaa085468e5ffa6e9
Dandy-Candra <106506074+Dandy-Candra@users.noreply.github.com>
2024-02-14 14:02:58 +0700

    change resto name

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Resto Dandy</a>
+            <a href="index.html">Terpesona Resto</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
